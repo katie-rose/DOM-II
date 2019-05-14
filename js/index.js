3d70f3f45b49262861a87396e404f1e3e3a45a29
Katie Rose <katiefelten@gmail.com>
2019-05-14 15:45:40 -0400

    added the rest of the event listeners for Task 3

--- js/index.js
+++ js/index.js
@@ -1,1 +1,50 @@
-// Your code goes here
+
+// mouseover
+const logo = document.querySelector(".logo-heading");
+logo.addEventListener('mouseover', (event) => {
+    logo.style.fontSize= '5rem';
+    TweenMax.to(".nav", 3, {x:-200, opacity: 1}) 
+    event.stopPropagation();
+});
+
+// keydown
+document.addEventListener('keydown', () => {
+    alert('Would you like to your 847284th email newsletter?');
+});
+
+// wheel
+introImg.addEventListener('wheel', () => {
+    introImg.style.height = 'auto';
+});
+
+// drag / drop
+
+// load
+window.addEventListener('load', () => {
+    console.log("All resources loaded bro");
+  });
+
+// focus
+
+// resize
+window.addEventListener('resize', () => {
+    alert(`Don't mess with Texas.`);
+});
+
+// scroll
+
+// select 
+
+// dblclick
+const introH2 = document.querySelector('.intro h2');
+introH2.addEventListener('dblclick', () => {
+    introH2.innerHTML = 'Golden Egg - here is your %20 coupon code #1goldenEggBounty';
+});
+
+// Prevent navigation from refreshing page
+const navTags = document.querySelectorAll('.nav-link');
+for (i=0; i<navTags.length; i++) {
+    navTags[i].addEventListener('click', (event) => {
+        event.preventDefault();
+    });
+};
